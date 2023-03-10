c8f90fa4f3264079fb8b53ceb514ef5598af8571
Pauliina Lehtikangas <pauliina.lehtikangas@outlook.com>
2023-03-10 14:52:34 +0200

    First working version of modern app UI-design

---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using ModernDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+     class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand FeaturedViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public FeaturedViewModel FeaturedVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        {
+            HomeVM = new HomeViewModel();
+            FeaturedVM = new FeaturedViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            FeaturedViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = FeaturedVM;
+            });
+        }
+    }
+}
